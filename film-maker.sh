2caa5510ad3800a406f43a9e28d297813b4dee9e
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2024-03-08 10:27:20 +0800

    Temp save

--- film-maker.sh
+++
@@ -1,8 +0,0 @@
-# for frame in range [0, 180)
-# execute: ~/SimCraft/build/FluidSim/gpu-smoke -o ./scenes/media -e {frame}
-# then execute: ./cmake-build-release/Core/rdcraft ~/frames/frame-{frame}.exr
-
-for frame in (seq 180 360)
-    ~/SimCraft/build/FluidSim/gpu-smoke -o ./scenes/media -e $frame
-    ./cmake-build-release/Core/rdcraft ~/frames/frame-$frame.exr
-end
